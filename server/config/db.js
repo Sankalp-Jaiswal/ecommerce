90b590c2af156e6c0deab45891fb7054eed3a047
Sankalp-Jaiswal <sankalpjaiswal264@gmail.com>
2025-01-29 09:44:39 +0530

    late

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
@@ -11,33 +14,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
-
-
-
-
-
-
-
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   mongoose.connection.on("connected", () => {
-//     console.log("connected to database");
-//   });
-
-//   await mongoose.connect(process.env.MONGODB_URI);
-
-//     try {
-//       const conn = await mongoose.connect(process.env.MONGO_URI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       });
-//       console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (err) {
-//       console.error(err);
-//       process.exit(1);
-//     }
-// };
-
-// export default connectDB;
