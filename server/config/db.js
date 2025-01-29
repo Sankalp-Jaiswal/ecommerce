fe0f96f20c18269d18fd342c748a27cfc8813ffa
Sankalp-Jaiswal <sankalpjaiswal264@gmail.com>
2025-01-29 09:38:02 +0530

    mongo db

--- server/config/db.js
+++ server/config/db.js
@@ -1,22 +1,43 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("connected to database");
-  });
-
-  await mongoose.connect(process.env.MONGODB_URI);
-
-  //   try {
-  //     const conn = await mongoose.connect(process.env.MONGO_URI, {
-  //       useNewUrlParser: true,
-  //       useUnifiedTopology: true,
-  //     });
-  //     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  //   } catch (err) {
-  //     console.error(err);
-  //     process.exit(1);
-  //   }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   mongoose.connection.on("connected", () => {
+//     console.log("connected to database");
+//   });
+
+//   await mongoose.connect(process.env.MONGODB_URI);
+
+//     try {
+//       const conn = await mongoose.connect(process.env.MONGO_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+//       console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (err) {
+//       console.error(err);
+//       process.exit(1);
+//     }
+// };
+
+// export default connectDB;
